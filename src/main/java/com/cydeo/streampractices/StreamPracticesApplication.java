edaa0203530fa71aa6fdc1b95d2ad63e12742959
mizgin <mizgincvk@gmail.com>
2022-10-08 15:38:01 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/streampractices/StreamPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.streampractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamPracticesApplication.class, args);
+    }
+
+}
